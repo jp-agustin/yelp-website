2b5ed7e549315e88e202d3cae1284b6865f88e04
jp-agustin <agustin.johnpatrick@gmail.com>
2019-02-02 21:54:08 +1100

    Refactored project to separate models

--- app.js
+++ app.js
@@ -1,20 +1,12 @@
 let express     = require("express"),
     app         = express(),
     bodyParser  = require("body-parser"),
-    mongoose    = require('mongoose');
+    mongoose    = require("mongoose"),
+    Campground = require("./models/campground");
 
 mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
-
-// SCHEMA SETUP
-let campgroundSchema = new mongoose.Schema({
-  name: String,
-  image: String,
-  description: String
-});
-
-let Campground = mongoose.model("Campground", campgroundSchema);
 
 // ROUTES
 app.get("/", function(req, res) {
