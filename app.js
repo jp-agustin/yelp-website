26d198337d670ea5daf96775c5fd7202fa47b6d4
jp-agustin <agustin.johnpatrick@gmail.com>
2019-02-06 22:38:07 +1100

    Added user model, basic setup and dependency for Auth

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ let express     = require("express"),
     app         = express(),
     bodyParser  = require("body-parser"),
     mongoose    = require("mongoose"),
-    Campground = require("./models/campground"),
-    Comment   = require("./models/comment"),
-    seedDB = require("./seeds");
+    passport    = require("passport"),
+    LocalStrategy = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose"),
+    Campground  = require("./models/campground"),
+    Comment     = require("./models/comment"),
+    User        = require("./models/user"),
+    seedDB      = require("./seeds");
 
 mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
-app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 // seedDB();
 
